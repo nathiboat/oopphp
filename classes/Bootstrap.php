ad2621e02444472705529a16141d2fbdb9224b6f
nathi.taengnim <nathi.taengnim@gimo.co.uk>
2018-03-08 09:41:55 +0000

    add controller action and view

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -5,45 +5,41 @@ class Bootstrap {
     private $action;
     private $request;
 
-    public function __construct($request)
-    {
+    public function __construct($request){
         $this->request = $request;
-        if ($this->request['controller'] == "") {
+        if($this->request['controller'] == ""){
             $this->controller = 'home';
         } else {
             $this->controller = $this->request['controller'];
         }
-        if ($this->request['action'] == "") {
+        if($this->request['action'] == ""){
             $this->action = 'index';
         } else {
             $this->action = $this->request['action'];
         }
-
     }
 
-    public function createController() {
-        //check Class
-        if(class_exists($this->controller))
-        {
+    public function createController(){
+        // Check Class
+        if(class_exists($this->controller)){
             $parents = class_parents($this->controller);
             // Check Extend
-            if(in_array("Controller", $parents)) {
-                if (method_exists($this->controller, $this->action)) {
+            if(in_array("Controller", $parents)){
+                if(method_exists($this->controller, $this->action)){
                     return new $this->controller($this->action, $this->request);
                 } else {
-                    //Method controller does not exist
-                    echo '<h1> Method does not exist </h1>';
+                    // Method Does Not Exist
+                    echo '<h1>Method does not exist</h1>';
                     return;
                 }
             } else {
-                //Base Controller does not exist
-                echo '<h1> Base Controller does not exist </h1>';
+                // Base Controller Does Not Exist
+                echo '<h1>Base controller not found</h1>';
                 return;
-
             }
         } else {
-            //Controller Class does not exist
-            echo '<h1> Controller Class does not exist </h1>';
+            // Controller Class Does Not Exist
+            echo '<h1>Controller class does not exist</h1>';
             return;
         }
     }
