91820c05176594a2d2fd56fe3fa5a672fea2d374
Nathi Taengnim <nathi.taengnim@gimo.co.uk>
2018-03-08 23:13:07 +0000

    add model

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Define DB Params
+define("DB_HOST", "192.168.10.10");
+define("DB_USER", "homestead");
+define("DB_PASS", "secret");
+define("DB_NAME", "shareboard");
+
+//Define URL
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http://localhost/oopphp.dev");
