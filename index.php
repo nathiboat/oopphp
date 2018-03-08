3040b7226abe1a3a57453906d52759e8607bb22c
nathi.taengnim <nathi.taengnim@gimo.co.uk>
2018-03-08 09:53:09 +0000

    add new controllers

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require ('classes/Bootstrap.php');
 require ('classes/Controller.php');
 
 require('controllers/home.php');
+require('controllers/shares.php');
+require('controllers/users.php');
 
 
 $bootstrap = new Bootstrap($_GET);
