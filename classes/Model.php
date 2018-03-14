f9ca772310ddd9ea7da1238c3ae410f68cec16d9
nathi.taengnim <nathi.taengnim@gimo.co.uk>
2018-03-14 10:30:38 +0000

    update insert last ID check

--- classes/Model.php
+++ classes/Model.php
@@ -39,4 +39,9 @@ abstract class Model{
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function lastInsertId()
+    {
+        return $this->dbh->lastInsertId();
+    }
 }
